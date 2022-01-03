d301b95f6df5f8500b2ffcf9314409549567a605
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-01-03 13:35:27 +0000

    Starting Movies Database

---
+++ section-9-more-on-objects/assets/scripts/objects.js
@@ -0,0 +1,49 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const addMovieHandler = function () {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    id: Math.random(),
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+  };
+
+  movies.push(newMovie);
+  renderMovies();
+};
+
+const renderMovies = function () {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+  } else {
+    movieList.classList.add('visible');
+  }
+
+  movieList.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement('li');
+    movieElement.textContent = movie.info.title;
+    movieList.append(movieElement);
+  });
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
